3d67f59821092d4121a620989f9a9f3f5767435a
Bruce Wong <Xenovia@Bruces-MacBook-Pro.local>
2016-04-08 19:58:53 -0700

    fixed

--- challenges/Multiples/multiples.rb
+++ challenges/Multiples/multiples.rb
@@ -1,2 +1,6 @@
-class SumOfMultiples; def initialize(*set); @set = *set; end; def self.to(num); SumOfMultiples.new(3,5).to(num); end; def to(num); (1...num).select { |multiple| @set.any? { |law| multiple % law == 0 } }.inject(0, :+); end; end
+class SumOfMultiples; 
+  def initialize(*set); @set = *set; end; 
+  def self.to(num); SumOfMultiples.new(3,5).to(num); end; 
+  def to(num); (1...num).select { |multiple| @set.any? { |law| multiple % law == 0 } }.inject(0, :+); end; 
+end
   
